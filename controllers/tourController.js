a21d5e06d237e9fd22edae7a666cabbea11e7b94
Your Name <leulgebremariam1995@gmail.com>
2026-09-10 18:04:32 +0300

    feat:implementing query filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,23 @@ const Tour = require('../models/tourModel');
 // Routes
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    console.log(req.query);
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    // console.log(req.query, queryObj);
+    const query = Tour.find(queryObj);
+
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+    const tours = await query;
     res.status(200).json({
       status: 'success',
 
