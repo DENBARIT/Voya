50575baf94e5ed50b7f0990c055db0f3842dc6c1
Your Name <leulgebremariam1995@gmail.com>
2026-09-15 11:42:33 +0300

    feat:pipline for aggregation of tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+
 exports.aliasTopTours = (req, res, next) => {
   res.locals.queryOptions = {
     limit: '5',
@@ -214,6 +215,61 @@ exports.deleteTour = async (req, res) => {
     });
   } catch (err) {
     res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+          numTours: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      {
+        $match: { _id: { $ne: 'EASY' } },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
+    });
+  } catch (err) {
+    res.stats(404).json({
       status: 'fail',
       message: err,
     });
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,6 +7,8 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getMonthlyPlan,
+  getTourStats,
 } = require('../controllers/tourController.js');
 
 const Router = express.Router();
@@ -25,6 +27,8 @@ const Router = express.Router();
 // });
 Router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 Router.route('/').get(getAllTours).post(createTour);
+Router.route('/tour-stats').get(getTourStats);
+Router.route('/tour-stats').get(getTourStats);
+Router.route('/monthly-plan/:year').get(getMonthlyPlan);
 Router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
 module.exports = Router;
